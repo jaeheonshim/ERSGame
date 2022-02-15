3f58d2fede46dd8f3906f8752e080073074b64c7
Jaeheon Shim <git@jaeheonshim.com>
2022-02-14 22:25:01 -0500

    add back button

--- core/src/com/jaeheonshim/ersgame/screen/JoinGameScreen.java
+++ core/src/com/jaeheonshim/ersgame/screen/JoinGameScreen.java
@@ -1,11 +1,16 @@
 package com.jaeheonshim.ersgame.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -18,6 +23,8 @@ public class JoinGameScreen implements Screen {
     private final ERSTextField nameField;
     private final ERSTextField codeField;
     private final ERSTextButton joinButton;
+    private final Button backButton;
+
     private ERSGame game;
     private Skin skin;
     private Stage stage;
@@ -30,28 +37,42 @@ public class JoinGameScreen implements Screen {
         stage = new Stage(new ExtendViewport(600, 900));
         table = new Table();
         table.setFillParent(true);
+//        table.setDebug(true);
         stage.addActor(table);
 
-        ERSLabel titleLabel = new ERSLabel("Join Game", skin, game);
-        titleLabel.setAlignment(Align.center);
-        table.top();
-        table.add(titleLabel).top().expand().fillX().padTop(32);
+        table.padTop(32);
+
+        backButton = new Button(skin, "back");
+
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(game.mainScreen);
+            }
+        });
+
+        table.add(backButton).center().left().padLeft(32).padTop(4).height(64).width(74);
+
+        ERSLabel titleLabel = new ERSLabel("Join Game", skin, "heading", game);
+        table.add(titleLabel).expandX().center().left().padLeft(28);
         table.row();
 
         table.center();
 
         nameField = new ERSTextField("", skin, game);
         nameField.setMessageText("Username");
-        table.add(nameField).fillX().padLeft(32).padRight(32);
+        table.add(nameField).expandY().fillX().padLeft(32).padRight(32).colspan(2).bottom();
         table.row();
 
         codeField = new ERSTextField("", skin, game);
         codeField.setMessageText("Join Code");
-        table.add(codeField).fillX().padLeft(32).padRight(32);
+        codeField.setTextFieldFilter(new TextField.TextFieldFilter.DigitsOnlyFilter());
+
+        table.add(codeField).fillX().padLeft(32).padRight(32).colspan(2);
         table.row();
 
         joinButton = new ERSTextButton("Join Game", skin, game);
-        table.add(joinButton).fillX().expandY().top().padLeft(38).padRight(38).padTop(16);
+        table.add(joinButton).fillX().expandY().top().padLeft(38).padRight(38).padTop(16).colspan(2);
     }
 
     @Override
