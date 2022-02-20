88a9c94c6596f50e44a837bd78f51afb435a32c3
Jaeheon Shim <git@jaeheonshim.com>
2022-02-20 00:07:27 -0500

    Reorder methods in Player

--- core/src/com/jaeheonshim/ersgame/game/model/Player.java
+++ core/src/com/jaeheonshim/ersgame/game/model/Player.java
@@ -15,43 +15,6 @@ public class Player {
     public Player() {
     }
 
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return Objects.equals(uuid, player.uuid);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(uuid);
-    }
-
-    public Iterator<CardType> getCards() {
-        return cardStack.iterator();
-    }
-
-    public int getCardCount() {
-        return cardCount;
-    }
-
     public void addCardToBottom(CardType cardType) {
         cardStack.addLast(cardType);
         cardCount++;
@@ -70,5 +33,42 @@ public class Player {
 
     public void removeCardStack() {
         cardStack = null;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Iterator<CardType> getCards() {
+        return cardStack.iterator();
+    }
+
+    public int getCardCount() {
+        return cardCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(uuid, player.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
     }
 }
