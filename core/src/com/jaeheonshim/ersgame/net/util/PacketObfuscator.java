691fe87511d50ca41066b06700146b2cf71ca1f0
Jaeheon Shim <git@jaeheonshim.com>
2022-02-21 17:08:42 -0500

    Test packet obfuscation

--- core/src/com/jaeheonshim/ersgame/net/util/PacketObfuscator.java
+++ core/src/com/jaeheonshim/ersgame/net/util/PacketObfuscator.java
@@ -3,12 +3,15 @@ package com.jaeheonshim.ersgame.net.util;
 import java.nio.charset.StandardCharsets;
 
 public class PacketObfuscator {
-    private static char KEY = 0xe7;
+    private static char KEY = 0x487a;
+    private static char INCREMENT = 0xF;
 
     public static String applyMask(String packet) {
+        char key = KEY;
         char[] chars = packet.toCharArray();
         for(int i = 0; i < chars.length; i++) {
-            chars[i] = (char) (chars[i] ^ KEY);
+            chars[i] = (char) (chars[i] ^ key);
+            key += INCREMENT;
         }
 
         return new String(chars);
